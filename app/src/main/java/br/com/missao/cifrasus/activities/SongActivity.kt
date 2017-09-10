9655a64f3dbbc639b6a6db02fdaa70449cb2b896
Tiago Missão <tiago_missao@bitbucket.org>
2017-09-10 09:27:30 -0300

    Enable user to increase/decrease chords font
    * Dialog View to increase/decrease chords font
    * Method to convert Pixel to SP

--- app/src/main/java/br/com/missao/cifrasus/activities/SongActivity.kt
+++ app/src/main/java/br/com/missao/cifrasus/activities/SongActivity.kt
@@ -1,7 +1,8 @@
 package br.com.missao.cifrasus.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.support.v7.widget.Toolbar
 import android.util.TypedValue
@@ -9,12 +10,10 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.BounceInterpolator
-import android.view.animation.LinearInterpolator
 import android.view.animation.OvershootInterpolator
 import android.widget.FrameLayout
+import android.widget.SeekBar
 import android.widget.TextView
-
 import br.com.missao.cifrasus.R
 import br.com.missao.cifrasus.app.AppPreferences
 import br.com.missao.cifrasus.extensions.*
@@ -30,329 +29,381 @@ import kotlinx.android.synthetic.main.activity_song.*
 import kotlinx.android.synthetic.main.appbar_song.*
 import javax.inject.Inject
 
+
 /**
  * Songs Activity View
  */
 class SongActivity : AppCompatActivity(), SongMvpRequiredViewOperations {
 
-  /**
-   * Presenter
-   */
-  @Inject lateinit var presenter: SongMvpPresenterOperations
+    /**
+     * Presenter
+     */
+    @Inject lateinit var presenter: SongMvpPresenterOperations
 
-  /**
-   * Logger to report errors
-   */
-  @Inject lateinit var logger: Logger
+    /**
+     * Logger to report errors
+     */
+    @Inject lateinit var logger: Logger
 
-  /**
-   * Song's data
-   */
-   var song: SongWrapper? = null
+    /**
+     * Song's data
+     */
+    var song: SongWrapper? = null
 
-  /**
-   * Song's font size defined by user
-   */
-  var contextFontSize = 0.0f
+    /**
+     * Song's font size defined by user
+     */
+    var contextFontSize = 0.0f
 
-  /**
-   * Indicates if layout was already measured
-   */
-  var isLayoutMeasured = false
+    /**
+     * Indicates if layout was already measured
+     */
+    var isLayoutMeasured = false
 
-  /**
-   * Indicates is the FAB is rotate
-   */
-  var isFabRotated = false
+    /**
+     * Indicates is the FAB is rotate
+     */
+    var isFabRotated = false
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_song)
-    inject()
-    setupComponents()
-    setupEvents()
-  }
+    /**
+     * Font Size Seek Bar
+     */
+    var seekFontSize: SeekBar? = null
 
-  /**
-   * Injects activity dependencies
-   */
-  fun inject() {
-    this.app.getDaggerViewComponent().inject(this)
-    presenter.setView(this)
-  }
+    /**
+     * Text Font Size
+     */
+    var textFontSize: TextView? = null
 
-  /**
-   * Configures View to be shown
-   */
-  fun setupComponents() {
-    contextFontSize = PixelDimensions.convertSpToPixels(resources.getDimension(R.dimen.fontsize_title), this.resources)
-
-    // It is necessary to cast appbarSong as Toolbar because the include tag on XML file. Since
-    // include tag loads a view type
-    this.setToolbar(appbarSong as Toolbar, showTitle = false, showHomeButton = false)
-  }
-
-  fun setupEvents() {
-    addMeasureListener()
-
-    fabOptions.setOnClickListener {
-      toggleOptions()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+        inject()
+        setupComponents()
+        setupEvents()
     }
 
-    frameBackground.setOnClickListener {
-      toggleOptions()
-    }
-  }
-
-  override fun onStart() {
-    super.onStart()
-    presenter.getSong(1)
-  }
-
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    menuInflater.inflate(R.menu.song_menu, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-    when (item.itemId) {
-      R.id.menuSongIncreaseFont -> { increaseFontSize(); return true }
-      R.id.menuSongDecreaseFont -> { decreaseFontSize(); return true }
+    /**
+     * Injects activity dependencies
+     */
+    fun inject() {
+        this.app.getDaggerViewComponent().inject(this)
+        presenter.setView(this)
     }
 
-    return super.onOptionsItemSelected(item)
-  }
+    /**
+     * Configures View to be shown
+     */
+    fun setupComponents() {
+        contextFontSize = PixelDimensions.convertSpToPixels(resources.getDimension(R.dimen.fontsize_title), this.resources)
 
-  /**
-   * Subscribes a layout listener
-   */
-  private fun addMeasureListener() {
-    textViewModel.afterMeasured {
-      isLayoutMeasured = true
-      displaySongPhrases()
+        // It is necessary to cast appbarSong as Toolbar because the include tag on XML file. Since
+        // include tag loads a view type
+        this.setToolbar(appbarSong as Toolbar, showTitle = false, showHomeButton = false)
     }
 
-    fabAddList.afterMeasured {
-      hide()
+    fun setupEvents() {
+        addMeasureListener()
+
+        fabOptions.setOnClickListener {
+            toggleOptions()
+        }
+
+        frameBackground.setOnClickListener {
+            toggleOptions()
+        }
+
+        fabFontSize.setOnClickListener {
+            toggleOptions()
+            buildTypographyDialog()
+        }
     }
 
-    fabChangeTone.afterMeasured {
-      hide()
+    override fun onStart() {
+        super.onStart()
+        presenter.getSong(1)
     }
 
-    fabFontSize.afterMeasured {
-      hide()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.song_menu, menu)
+        return true
     }
 
-    cardAddList.afterMeasured {
-      showCardOptions(cardAddList, false)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menuSongIncreaseFont -> {
+                increaseFontSize(); return true
+            }
+            R.id.menuSongDecreaseFont -> {
+                decreaseFontSize(); return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
-    cardChangeTone.afterMeasured {
-      showCardOptions(cardChangeTone, false)
+    /**
+     * Subscribes a layout listener
+     */
+    private fun addMeasureListener() {
+        textViewModel.afterMeasured {
+            isLayoutMeasured = true
+            displaySongPhrases()
+        }
+
+        fabAddList.afterMeasured {
+            hide()
+        }
+
+        fabChangeTone.afterMeasured {
+            hide()
+        }
+
+        fabFontSize.afterMeasured {
+            hide()
+        }
+
+        cardAddList.afterMeasured {
+            showCardOptions(cardAddList, false)
+        }
+
+        cardChangeTone.afterMeasured {
+            showCardOptions(cardChangeTone, false)
+        }
+
+        cardFontSize.afterMeasured {
+            showCardOptions(cardFontSize, false)
+        }
+
     }
 
-    cardFontSize.afterMeasured {
-      showCardOptions(cardFontSize, false)
+    /**
+     * Measures and displays Song's data without ellipsize its content
+     */
+    private fun displaySongPhrases() {
+        val phrases = song?.phrases ?: listOf()
+
+        if (phrases.isEmpty()) return
+        val charSize = textViewModel.getCharacterPixelAverage()
+        var measuredPhrases = arrayListOf<PhraseWrapper>()
+
+        phrases.forEach { measuredPhrases.addAll(PhraseSplitter.measurePhrase(textViewModel, it)) }
+
+        clearContainerData()
+
+        measuredPhrases.forEach {
+            val chordsLine = inflateChordLayout()
+
+            if (it.chords.isNotEmpty()) {
+                it.chords.forEach { chordsLine.addView(buildChordTextView(it, charSize)) }
+
+            } else {
+                // This empty chord line is add to keep the song's chords easy to read
+                chordsLine.addView(buildChordTextView(buildEmptyChordWrapper(), charSize))
+            }
+
+            container.addView(chordsLine)
+            container.addView(buildPhraseLayout(it))
+        }
     }
 
-  }
-
-  /**
-   * Measures and displays Song's data without ellipsize its content
-   */
-  private fun displaySongPhrases() {
-    val phrases = song?.phrases ?: listOf()
-
-    if (phrases.isEmpty()) return
-    val charSize = textViewModel.getCharacterPixelAverage()
-    var measuredPhrases = arrayListOf<PhraseWrapper>()
-
-    phrases.forEach { measuredPhrases.addAll(PhraseSplitter.measurePhrase(textViewModel, it)) }
-
-    clearContainerData()
-
-    measuredPhrases.forEach {
-        val chordsLine = inflateChordLayout()
-
-      if (it.chords.isNotEmpty()) {
-        it.chords.forEach { chordsLine.addView(buildChordTextView(it, charSize)) }
-
-      } else {
-        // This empty chord line is add to keep the song's chords easy to read
-        chordsLine.addView(buildChordTextView(buildEmptyChordWrapper(), charSize))
-      }
-
-      container.addView(chordsLine)
-      container.addView(buildPhraseLayout(it))
-    }
-  }
-
-  /**
-   * Inflates the chord root layout
-   */
-  private fun inflateChordLayout() : FrameLayout {
-    return container.inflate(R.layout.empty_frame, false) as FrameLayout
-  }
-
-  /**
-   * Builds and configures a [TextView] to display a [chord]
-   */
-  private fun buildChordTextView(chord: ChordWrapper, charSize: Int) : TextView {
-    val textView = container.inflate(R.layout.partial_chord, false) as TextView
-    textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, contextFontSize)
-    textView.typeface = textViewModel.typeface
-    textView.text = chord.value
-    textView.setPadding(charSize * chord.placement, 0, 0, 0)
-    return textView
-  }
-
-  /**
-   * Builds and configures the layout to display [phrase]
-   */
-  private fun buildPhraseLayout(phrase: PhraseWrapper) : View {
-    val view = container.inflate(R.layout.partial_phrase, false)
-    val textView = view.findViewById<TextView>(R.id.textPhrase)
-    textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, contextFontSize)
-    textView.typeface = textViewModel.typeface
-    textView.text = phrase.text
-    return view
-  }
-
-  /**
-   *  Builds a Empty ChordWrapper
-   */
-  private fun buildEmptyChordWrapper() : ChordWrapper {
-    return ChordWrapper("", 0)
-  }
-
-  /**
-   * Removes previous container's data
-   */
-  private fun clearContainerData() {
-    container.removeAllViews()
-  }
-
-  /**
-   * Increases one SP in font size
-   */
-  private fun increaseFontSize() {
-    textViewModel.setTextSize(TypedValue.COMPLEX_UNIT_SP, ++contextFontSize)
-    displaySongPhrases()
-  }
-
-  /**
-   * Decreases one SP in font size
-   */
-  private fun decreaseFontSize() {
-    if (contextFontSize - 1 < 1) return
-
-    textViewModel.setTextSize(TypedValue.COMPLEX_UNIT_SP, --contextFontSize)
-    displaySongPhrases()
-  }
-
-  /**
-   * Toggles FAB's state
-   */
-  private fun toggleOptions() {
-    var degrees = -45f
-    if (isFabRotated) {
-      degrees *= -1
+    /**
+     * Inflates the chord root layout
+     */
+    private fun inflateChordLayout(): FrameLayout {
+        return container.inflate(R.layout.empty_frame, false) as FrameLayout
     }
 
-    isFabRotated = !isFabRotated
-
-    fabOptions.animate()
-        .rotationBy(degrees)
-        .setInterpolator(AccelerateDecelerateInterpolator())
-        .setDuration(AppPreferences.ANIMATION_FAST)
-        .start()
-
-
-    showActionOptions(isFabRotated)
-    showBackgroundView(isFabRotated)
-  }
-
-  /**
-   * Shows/Hides song action options
-   */
-  private fun showActionOptions(display: Boolean) {
-    if (display) {
-      fabAddList.show()
-      fabFontSize.show()
-      fabChangeTone.show()
-
-    } else {
-      fabAddList.hide()
-      fabFontSize.hide()
-      fabChangeTone.hide()
+    /**
+     * Builds and configures a [TextView] to display a [chord]
+     */
+    private fun buildChordTextView(chord: ChordWrapper, charSize: Int): TextView {
+        val textView = container.inflate(R.layout.partial_chord, false) as TextView
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, contextFontSize)
+        textView.typeface = textViewModel.typeface
+        textView.text = chord.value
+        textView.setPadding(charSize * chord.placement, 0, 0, 0)
+        return textView
     }
 
-    showCardOptions(cardAddList, display)
-    showCardOptions(cardFontSize, display)
-    showCardOptions(cardChangeTone, display)
-  }
-
-  private fun showCardOptions(card: CardView, display: Boolean) {
-
-    if ( display ) {
-      card.scaleX = 0f
-      card.scaleY = 0f
-      card.visibility = View.VISIBLE
-
-      card.animate()
-          .scaleYBy(1f)
-          .scaleXBy(1f)
-          .setInterpolator(OvershootInterpolator())
-          .setDuration(AppPreferences.ANIMATION_FAST)
-          .start()
-
-    } else {
-      card.animate()
-          .scaleYBy(-1f)
-          .scaleXBy(-1f)
-          .withEndAction { frameBackground.visibility = View.GONE }
-          .setInterpolator(AccelerateDecelerateInterpolator())
-          .setDuration(AppPreferences.ANIMATION_FAST)
-          .start()
+    /**
+     * Builds and configures the layout to display [phrase]
+     */
+    private fun buildPhraseLayout(phrase: PhraseWrapper): View {
+        val view = container.inflate(R.layout.partial_phrase, false)
+        val textView = view.findViewById<TextView>(R.id.textPhrase)
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, contextFontSize)
+        textView.typeface = textViewModel.typeface
+        textView.text = phrase.text
+        return view
     }
-  }
 
-  /**
-   * Shows/Hides background options view
-   */
-  private fun showBackgroundView(display: Boolean) {
-
-    if ( display ) {
-      frameBackground.alpha = 0f
-      frameBackground.visibility = View.VISIBLE
-
-      frameBackground.animate()
-          .alphaBy(1f)
-          .setInterpolator(AccelerateDecelerateInterpolator())
-          .setDuration(AppPreferences.ANIMATION_FAST)
-          .start()
-
-    } else {
-      frameBackground.animate()
-          .alphaBy(-1f)
-          .withEndAction { frameBackground.visibility = View.GONE }
-          .setInterpolator(AccelerateDecelerateInterpolator())
-          .setDuration(AppPreferences.ANIMATION_FAST)
-          .start()
+    /**
+     *  Builds a Empty ChordWrapper
+     */
+    private fun buildEmptyChordWrapper(): ChordWrapper {
+        return ChordWrapper("", 0)
     }
-  }
 
-  /**
-   * Obtains song's data
-   */
-  override fun onGetSong(music: SongWrapper) {
-    song = music
-    textSong.text = music.name
-    textArtist.text = music.artist
-    if (isLayoutMeasured) {
-      displaySongPhrases()
+    /**
+     * Removes previous container's data
+     */
+    private fun clearContainerData() {
+        container.removeAllViews()
     }
-  }
+
+    /**
+     * Increases one SP in font size
+     */
+    private fun increaseFontSize() {
+        textViewModel.setTextSize(TypedValue.COMPLEX_UNIT_SP, ++contextFontSize)
+        displaySongPhrases()
+    }
+
+    /**
+     * Decreases one SP in font size
+     */
+    private fun decreaseFontSize() {
+        if (contextFontSize - 1 < 1) return
+
+        textViewModel.setTextSize(TypedValue.COMPLEX_UNIT_SP, --contextFontSize)
+        displaySongPhrases()
+    }
+
+    /**
+     * Toggles FAB's state
+     */
+    private fun toggleOptions() {
+        var degrees = -45f
+        if (isFabRotated) {
+            degrees *= -1
+        }
+
+        isFabRotated = !isFabRotated
+
+        fabOptions.animate()
+                .rotationBy(degrees)
+                .setInterpolator(AccelerateDecelerateInterpolator())
+                .setDuration(AppPreferences.ANIMATION_FAST)
+                .start()
+
+
+        showActionOptions(isFabRotated)
+        showBackgroundView(isFabRotated)
+    }
+
+    /**
+     * Shows/Hides song action options
+     */
+    private fun showActionOptions(display: Boolean) {
+        if (display) {
+            fabAddList.show()
+            fabFontSize.show()
+            fabChangeTone.show()
+
+        } else {
+            fabAddList.hide()
+            fabFontSize.hide()
+            fabChangeTone.hide()
+        }
+
+        showCardOptions(cardAddList, display)
+        showCardOptions(cardFontSize, display)
+        showCardOptions(cardChangeTone, display)
+    }
+
+    private fun showCardOptions(card: CardView, display: Boolean) {
+
+        if (display) {
+            card.scaleX = 0f
+            card.scaleY = 0f
+            card.visibility = View.VISIBLE
+
+            card.animate()
+                    .scaleYBy(1f)
+                    .scaleXBy(1f)
+                    .setInterpolator(OvershootInterpolator())
+                    .setDuration(AppPreferences.ANIMATION_FAST)
+                    .start()
+
+        } else {
+            card.animate()
+                    .scaleYBy(-1f)
+                    .scaleXBy(-1f)
+                    .withEndAction { frameBackground.visibility = View.GONE }
+                    .setInterpolator(AccelerateDecelerateInterpolator())
+                    .setDuration(AppPreferences.ANIMATION_FAST)
+                    .start()
+        }
+    }
+
+    /**
+     * Shows/Hides background options view
+     */
+    private fun showBackgroundView(display: Boolean) {
+
+        if (display) {
+            frameBackground.alpha = 0f
+            frameBackground.visibility = View.VISIBLE
+
+            frameBackground.animate()
+                    .alphaBy(1f)
+                    .setInterpolator(AccelerateDecelerateInterpolator())
+                    .setDuration(AppPreferences.ANIMATION_FAST)
+                    .start()
+
+        } else {
+            frameBackground.animate()
+                    .alphaBy(-1f)
+                    .withEndAction { frameBackground.visibility = View.GONE }
+                    .setInterpolator(AccelerateDecelerateInterpolator())
+                    .setDuration(AppPreferences.ANIMATION_FAST)
+                    .start()
+        }
+    }
+
+    /**
+     * Builds Material Dialog to adjust typographic
+     */
+    private fun buildTypographyDialog() {
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_font_size, null).apply {
+            seekFontSize = findViewById<SeekBar>(R.id.seekFontSize)
+            textFontSize = findViewById<TextView>(R.id.textLyrics)
+        }
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogStyle).apply {
+            this.setView(dialogView)
+            this.setTitle(getString(R.string.select_font_size))
+            this.setPositiveButton(getString(R.string.positive_button_text), null)
+        }
+
+        seekFontSize?.progress = contextFontSize.toInt()
+        textFontSize?.text = song?.phrases?.first()?.text
+
+        seekFontSize?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                contextFontSize = progress.toFloat()
+                textFontSize?.setTextSize(TypedValue.COMPLEX_UNIT_SP, contextFontSize)
+                textViewModel.setTextSize(TypedValue.COMPLEX_UNIT_SP, contextFontSize)
+                displaySongPhrases()
+            }
+        })
+
+        builder.show()
+    }
+
+    /**
+     * Obtains song's data
+     */
+    override fun onGetSong(music: SongWrapper) {
+        song = music
+        textSong.text = music.name
+        textArtist.text = music.artist
+        if (isLayoutMeasured) {
+            displaySongPhrases()
+        }
+    }
 }
--- app/src/main/java/br/com/missao/cifrasus/utils/PixelDimensions.kt
+++ app/src/main/java/br/com/missao/cifrasus/utils/PixelDimensions.kt
@@ -8,27 +8,34 @@ import android.util.TypedValue
  */
 object PixelDimensions {
 
-  /**
-   * Converts dp to pixels
-   */
-  fun convertDpToPixels(dp: Float, resource: Resources): Float {
-    val px = TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP, dp, resource.displayMetrics)
-    return px
-  }
+    /**
+     * Converts dp to pixels
+     */
+    fun convertDpToPixels(dp: Float, resource: Resources): Float {
+        val px = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp, resource.displayMetrics)
+        return px
+    }
 
-  /**
-   * Converts pixels to dp
-   */
-  fun converPixelsToDp(px: Float, resource: Resources): Float {
-    val dp = px / resource.displayMetrics.density
-    return dp
-  }
+    /**
+     * Converts pixels to dp
+     */
+    fun converPixelsToDp(px: Float, resource: Resources): Float {
+        val dp = px / resource.displayMetrics.density
+        return dp
+    }
 
-  /**
-   * Converts sp to pixels
-   */
-  fun convertSpToPixels(sp: Float, resource: Resources): Float {
-    return sp / resource.displayMetrics.scaledDensity
-  }
+    /**
+     * Converts sp to pixels
+     */
+    fun convertSpToPixels(sp: Float, resource: Resources): Float {
+        return sp / resource.displayMetrics.scaledDensity
+    }
+
+    /**
+     * Converts pixels to sp
+     */
+    fun convertPixelsToSp(pixels: Float, resource: Resources): Float {
+        return pixels * resource.displayMetrics.scaledDensity
+    }
 }
